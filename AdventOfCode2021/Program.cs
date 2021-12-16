590794d5902b714032c82e9e1861834338a14352
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2021-12-16 11:23:11 -0800

    Add missing using statements

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode2021
