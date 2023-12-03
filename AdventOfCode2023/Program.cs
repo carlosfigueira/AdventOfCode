ca7f9e99b5a94daf3dfd90ebe4cf8edf9edc6c1a
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2023-12-03 07:34:39 -0800

    AoC2023 - day 3

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -13,7 +13,8 @@ namespace AdventOfCode2023
         static void Main(string[] args)
         {
             // Day1.Solve();
-            Day2.Solve();
+            // Day2.Solve();
+            Day3.Solve();
         }
     }
 
@@ -180,6 +181,141 @@ namespace AdventOfCode2023
             }
 
             Console.WriteLine("Day 2, part 2: " + powerSumPart2);
+        }
+    }
+
+    class Day3
+    {
+        static bool IsSymbol(char c)
+        {
+            return !char.IsDigit(c) && c != '.';
+        }
+
+        class NumberDef
+        {
+            public int Value { get; }
+            public int Line { get; }
+            public int StartCol { get; }
+            public int EndCol { get; }
+            public bool IsPart { get; }
+            public NumberDef(int value, int line, int startCol, int endCol, bool isPart)
+            {
+                Value = value;
+                Line = line;
+                StartCol = startCol;
+                EndCol = endCol;
+                IsPart = isPart;
+            }
+        }
+
+        public static void Solve()
+        {
+            var useSample = false;
+            var lines = useSample ?
+                new[] { "467..114..", "...*......", "..35..633.", "......#...", "617*......", ".....+.58.", "..592.....", "......755.", "...$.*....", ".664.598.." } :
+                Helpers.LoadInput("day3.txt");
+
+            int sumParts = 0;
+            var numberDefsPerLine = new List<List<NumberDef>>();
+
+            for (int l = 0; l < lines.Length; l++)
+            {
+                numberDefsPerLine.Add(new List<NumberDef>());
+                var line = lines[l];
+                var startNumber = -1;
+                var number = 0;
+                for (int c = 0; c < line.Length; c++)
+                {
+                    if (char.IsDigit(line[c]))
+                    {
+                        if (startNumber < 0)
+                        {
+                            startNumber = c;
+                        }
+
+                        number *= 10;
+                        number += line[c] - '0';
+                    }
+
+                    if (!char.IsDigit(line[c]) || c == line.Length - 1)
+                    {
+                        var minCol = Math.Max(0, startNumber - 1);
+                        var maxCol = Math.Min(line.Length - 1, c);
+                        var isPart = false;
+                        if (startNumber > 0 && IsSymbol(lines[l][startNumber - 1]))
+                        {
+                            isPart = true;
+                        }
+                        else if (c < line.Length - 1 && IsSymbol(lines[l][c]))
+                        {
+                            isPart = true;
+                        }
+                        else
+                        {
+                            for (var col = minCol; col <= maxCol; col++)
+                            {
+                                if (l > 0 && IsSymbol(lines[l - 1][col]))
+                                {
+                                    isPart = true;
+                                    break;
+                                }
+
+                                if (l < lines.Length - 1 && IsSymbol(lines[l + 1][col]))
+                                {
+                                    isPart = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (startNumber >= 0)
+                        {
+                            numberDefsPerLine[l].Add(new NumberDef(number, l, startNumber, c - 1, isPart));
+                        }
+
+                        if (isPart)
+                        {
+                            sumParts += number;
+                        }
+
+                        startNumber = -1;
+                        number = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine("Day 3, part 1: " + sumParts);
+
+            var sumGearRatios = 0;
+            for (var l = 0; l < lines.Length; l++)
+            {
+                for (var c = 0; c < lines[l].Length; c++)
+                {
+                    if (lines[l][c] == '*')
+                    {
+                        var neighbooringNumbers = new List<NumberDef>();
+                        var minLine = Math.Max(0, l - 1);
+                        var maxLine = Math.Min(lines.Length - 1, l + 1);
+                        for (var line = minLine; line <= maxLine; line++)
+                        {
+                            foreach (var numberDef in numberDefsPerLine[line])
+                            {
+                                if ((numberDef.StartCol - 1) <= c && c <= (numberDef.EndCol + 1))
+                                {
+                                    neighbooringNumbers.Add(numberDef);
+                                }
+                            }
+                        }
+
+                        if (neighbooringNumbers.Count == 2)
+                        {
+                            sumGearRatios += neighbooringNumbers[0].Value * neighbooringNumbers[1].Value;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Day 3, part 2: " + sumGearRatios);
         }
     }
 
