8a9ef11f848d9d046ff15a038d22c400b1d32c41
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2024-12-02 15:44:11 -0800

    Advent of code 2024 - first 2 days

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,152 @@
+﻿namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day1.Solve();
+            Day2.Solve();
+        }
+    }
+
+    class Day1
+    {
+        public static void Solve()
+        {
+            var sampleInput = new[] { "3   4", "4   3", "2   5", "1   3", "3   9", "3   3" };
+
+            var useSample = false;
+            var lines = useSample ? sampleInput : Helpers.LoadInput("day1.txt");
+
+            var ids = lines
+                .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(i => i.Trim()).ToArray())
+                .Select(arr => new { left = arr[0], right = arr[1] });
+            var leftIds = ids.Select(i => int.Parse(i.left)).ToList();
+            leftIds.Sort();
+            var rightIds = ids.Select(i => int.Parse(i.right)).ToList();
+            rightIds.Sort();
+            var sum = 0;
+            for (int i = 0; i < leftIds.Count; i++)
+            {
+                sum += Math.Abs(leftIds[i] - rightIds[i]);
+            }
+
+            Console.WriteLine(sum);
+
+            var similarity = 0;
+            var previous = int.MinValue;
+            var previousSimilarity = -1;
+            for (var i = 0; i < leftIds.Count; i++)
+            {
+                var left = leftIds[i];
+                if (left == previous)
+                {
+                    similarity += previousSimilarity;
+                    continue;
+                }
+
+                var rightIndex = rightIds.BinarySearch(left);
+                if (rightIndex < 0)
+                {
+                    previousSimilarity = -1;
+                    previous = int.MinValue;
+                    continue;
+                }
+
+                var before = 0;
+                for (var j = rightIndex - 1; j >= 0 && rightIds[j] == rightIds[rightIndex]; j--) before++;
+                var after = 0;
+                for (var j = rightIndex + 1; j < rightIds.Count && rightIds[j] == rightIds[rightIndex]; j++) after++;
+                var currentSimilarity = (before + 1 + after) * left;
+
+                similarity += currentSimilarity;
+
+                previous = left;
+                previousSimilarity = currentSimilarity;
+            }
+
+            Console.WriteLine(similarity);
+        }
+    }
+
+    class Day2
+    {
+        public static void Solve()
+        {
+            var sampleInput = new[]
+            {
+                "7 6 4 2 1",
+                "1 2 7 8 9",
+                "9 7 6 2 1",
+                "1 3 2 4 5",
+                "8 6 4 4 1",
+                "1 3 6 7 9"
+            };
+
+            var useSample = false;
+            var lines = useSample ? sampleInput : Helpers.LoadInput("day2.txt");
+
+            var values = lines
+                .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i.Trim())).ToArray());
+
+            var safeCount = 0;
+            foreach (var line in values)
+            {
+                if (IsSafe(line)) safeCount++;
+            }
+
+            Console.WriteLine(safeCount);
+
+            var safeWithDampenerCount = 0;
+            // Could be smarter, but works well enough for this input
+            foreach (var line in values)
+            {
+                if (IsSafe(line))
+                {
+                    safeWithDampenerCount++;
+                }
+                else
+                {
+                    var isSafe = false;
+                    for (var i = 0; i < line.Length; i++)
+                    {
+                        if (IsSafe(line.Where((value, index) => index != i).ToArray()))
+                        {
+                            isSafe = true;
+                            break;
+                        }
+                    }
+
+                    if (isSafe)
+                    {
+                        safeWithDampenerCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(safeWithDampenerCount);
+        }
+
+        static bool IsSafe(int[] array)
+        {
+            if (array.Length < 2) return true;
+            var ascending = array[0] < array[1];
+            for (int i = 1; i < array.Length; i++)
+            {
+                var diff = ascending ? (array[i] - array[i - 1]) : (array[i - 1] - array[i]);
+                if (diff < 1 || diff > 3) return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class Helpers
+    {
+        public static string[] LoadInput(string fileName)
+        {
+            return File.ReadAllLines(Path.Combine("inputs", fileName));
+        }
+    }
+
+}
